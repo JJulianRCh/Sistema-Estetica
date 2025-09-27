3b6c76570465ea8c3833f5eb4b99bd8b887bcd17
Julian <jjulian.contact@gmail.com>
2025-09-26 22:36:01 -0600

    Se agrego sanitizacion y validacion de datos en el route del register

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,17 +1,56 @@
 import clientPromise from '@/lib/mongo';
 import bcrypt from 'bcryptjs';
 import { NextResponse } from 'next/server';
+import { sanitize } from 'mongo-sanitize'; // Para sanitizar los valores
+
+// Con esto validamos el formato del email
+// Se asegura que sea de tipo string y que cumpla con la expresion
+function validarEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return typeof email === "string" && regex.test(email);
+}
 
 export async function POST(request) {
   try {
     const client = await clientPromise;
     const db = client.db('estetica-citas');
     
-    const { nombre, apellido, email, telefono, password } = await request.json();
+    //const { nombre, apellido, email, telefono, password } = await request.json();
+    const body = await request.json();
 
+    // Sanitizamos los valores para evitar inyecciones
+    const nombre = sanitize(body.nombre);
+    const apellido = sanitize(body.apellido);
+    const email = sanitize(body.email);
+    const telefono = sanitize(body.telefono);
+    const password = sanitize(body.password);
+
+
+    /*
     if (!nombre || !email || !password) {
       return NextResponse.json(
         { success: false, message: 'Nombre, email y contraseña son obligatorios' },
+        { status: 400 }
+      );
+    }
+    */
+
+    // Se validan que los datos cumplan con los requisitos mínimos
+    if (
+      typeof nombre !== 'string' || nombre.length < 2
+      || typeof email !== 'string' || !validarEmail(email)
+      || typeof password !== 'string'
+    ) {
+      return NextResponse.json(
+        { success: false, message: 'Datos inválidos o incompletos' },
+        { status: 400 }
+      );
+    }
+
+    // Se aumento el minimo de 6 a 8 caracteres para mayor seguridad
+    if (password.length < 8) {
+      return NextResponse.json(
+        { success: false, message: 'La contraseña debe tener al menos 6 caracteres' },
         { status: 400 }
       );
     }
@@ -24,7 +63,7 @@ export async function POST(request) {
       );
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 12); // 12 rondas de salt en vez de 10
     
     const newUser = {
       nombre,
